69ed4a057301c40594bd390fdf60d52cbc8cc99f
Kashall <17620516+Kashalls@users.noreply.github.com>
2022-08-23 01:16:25 -0700

    More things wow

--- src/commands/catgirl.js
+++ src/commands/catgirl.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,5 +15,21 @@ module.exports = {
 };
 
 async function findCatGirlResponse() {
+	let image = '';
+	try {
+		const location = path.join(process.cwd(), 'images');
+		const files = fs.readdirSync(location);
+		const rand = Math.floor(Math.random() * files.length) + 1;
+		image = fs.readFileSync(path.join(location, files[rand]));
+	} catch (error) {
+		console.log(error);
+	}
 
+	if (image.length === 0) {
+		return 'Oops.';
+	} else {
+		const attachment = new AttachmentBuilder(image, { name: 'catgirl.png', description: 'I\'ts a catgirl. What else did you expect to be here?' });
+		return { attachments: [attachment] };
+	}
 }
+
