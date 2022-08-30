233cd5ae4b762d743345d3f1940898d648c02ebb
Raven Ravener <ravener.anime@gmail.com>
2022-08-30 08:25:26 +0430

    refactor: AkiClient -> KiriClient

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const path = require('path');
 
 const { GatewayIntentBits } = require('discord.js');
-const AkiClient = require('./library/KiriClient');
+const KiriClient = require('./library/KiriClient');
 
-const client = new AkiClient({ intents: [GatewayIntentBits.Guilds] });
+const client = new KiriClient({ intents: [GatewayIntentBits.Guilds] });
 
 client.keys = {
 	private: path.join(process.cwd(), 'keys', 'jwt.key'),
